2aafded8be40b5c9a43ba48e5267c8b91a11e442
Dagmawi Mulualem <87197602+DagmawiMulualem@users.noreply.github.com>
2025-07-29 04:36:14 -0400

    Update server.js

--- server.js
+++ server.js
@@ -140,8 +140,9 @@ app.post('/contact', async (req, res) => {
 
 // ---------- 404
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+  res.status(404).send('404 - Page Not Found');
 });
+
 
 // ---------- Start
 app.listen(PORT, () => {
